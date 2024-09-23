b4ac3fdea86a130b0141c91e04e0a40f39ff9533
Saihaj Mann <saihajmann07@gmail.com>
2024-09-22 22:49:25 -0600

    passing data

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
+'use client'
+
 import Image from "next/image";
 import D3Chart from "@/component/sunburst";
+import { useState } from "react";
 
 export default function Home() {
+  const {data, setData} = useState([10, 20, 30, 40, 50])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <input/>
-      <D3Chart/>
+      <D3Chart data={data} />
     </div>
   );
 }
