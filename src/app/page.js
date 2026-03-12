b60e68c70d5db8f189f368af896bbb7b4613d13a
RisingSatalite <saihajmann07@gmail.com>
2026-03-12 09:42:45 -0600

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -53,7 +53,7 @@ export default function Home() {
   }
 
   return (
-    <div class="grid items-center justify-items-center">
+    <div className="grid items-center justify-items-center">
       <div>
         <input
           type="file"
